b345ce8dbda3b05c6f327628a85053efece84653
YuChuXi <cyu245656@gmail.com>
2025-04-18 23:10:45 +0800

    a

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #unset no_proxy http_proxy https_proxy all_proxy
 #unset NO_PROXY HTTP_PROXY HTTPS_PROXY ALL_PROXY
+export PYTORCH_HIP_ALLOC_CONF=expandable_segments:True
 source $PWD/.venv/bin/activate
 python $PWD/main.py --port 48189 --listen 0.0.0.0 \
     --force-fp16 --fp16-unet --bf16-vae --fp16-text-enc \
-    --preview-method taesd --use-split-cross-attention \
+    --preview-method taesd \
     --lowvram --verbose --fast \
+    --use-quad-cross-attention \
+    #--use-split-cross-attention \
     #--tls-keyfile key.pem --tls-certfile cert.pem
 
