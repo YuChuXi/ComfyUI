d240675fbe6b6ac1d1427397132ae642abcf3af8
YuChuXi <cyu245656@gmail.com>
2025-08-07 00:11:46 +0800

    修复启动脚本中的参数配置，确保正确设置 MIOPEN_FIND_MODE 和低内存选项

--- start.sh
+++ start.sh
@@ -5,18 +5,22 @@ export PYTORCH_HIP_ALLOC_CONF=expandable_segments:True
 export FLASH_ATTENTION_TRITON_AMD_ENABLE=TRUE
 export HIPDNN_BENCHMARK=1
 export CUDNN_BENCHMARK=1
-export MIOPEN_FIND_ENFORCE=3
+export MIOPEN_FIND_MODE=2
+export HSA_OVERRIDE_GFX_VERSION=11.0.0
+export HIP_VISIBLE_DEVICES=0
+export MIOPEN_LOG_LEVEL=5
 
 
 source $PWD/.venv/bin/activate
 python $PWD/main.py --port 48189 --listen 0.0.0.0 \
-    --fp16-unet --bf16-vae --fp16-text-enc \
+    --fp16-unet --fp16-vae --fp16-text-enc \
     --preview-method taesd \
-    --novram \
+    --lowvram \
     --verbose DEBUG \
     --tls-keyfile key.pem --tls-certfile cert.pem \
     --async-offload \
     --use-flash-attention \
+    
     #--use-split-cross-attention \
     #--use-pytorch-cross-attention \
     #--use-quad-cross-attention \
