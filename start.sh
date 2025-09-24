e926b5cee2031f79ebf0f80118b908a4a1acd9ab
YuChuXi <cyu245656@gmail.com>
2025-09-24 13:54:28 +0800

    更新 .gitignore 文件以忽略 .bak 文件，注释掉 start.sh 中的环境变量设置和部分启动参数

--- start.sh
+++ start.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 #unset no_proxy http_proxy https_proxy all_proxy
 #unset NO_PROXY HTTP_PROXY HTTPS_PROXY ALL_PROXY
-export PYTORCH_HIP_ALLOC_CONF=expandable_segments:True
-export FLASH_ATTENTION_TRITON_AMD_ENABLE=TRUE
-export HIPDNN_BENCHMARK=1
+#export PYTORCH_HIP_ALLOC_CONF=expandable_segments:True
+#export FLASH_ATTENTION_TRITON_AMD_ENABLE=TRUE
+#export HIPDNN_BENCHMARK=1
 export CUDNN_BENCHMARK=1
-export MIOPEN_FIND_MODE=2
-export HSA_OVERRIDE_GFX_VERSION=11.0.0
-export ROCR_VISIBLE_DEVICES=0
-export GPU_DEVICE_ORDINAL=0
+#export MIOPEN_FIND_MODE=2
+#export HSA_OVERRIDE_GFX_VERSION=11.0.0
+#export ROCR_VISIBLE_DEVICES=0
+#export GPU_DEVICE_ORDINAL=0
 
-
-python $PWD/main.py --port 48189 --listen 0.0.0.0 \
-    --fp16-unet --fp32-vae --fp16-text-enc \
+# python $PWD/main.py --port 48189 --listen 0.0.0.0 \
+python $PWD/main.py --port 60080 --listen 0.0.0.0 \
+    --fp16-unet --bf16-vae --fp16-text-enc \
     --preview-method taesd \
     --novram \
     --verbose DEBUG \
     --tls-keyfile key.pem --tls-certfile cert.pem \
     --async-offload \
-    --disable-xformer \
-    --use-pytorch-cross-attention \
+    #--use-pytorch-cross-attention \
     #--use-flash-attention \
     #--use-split-cross-attention \
+    #--disable-xformer \
     #--use-quad-cross-attention \
+    #--use-sage-attention \
 
