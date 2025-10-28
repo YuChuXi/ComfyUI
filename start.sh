722811f056ea9df11b72337040e2c399d9bb94f3
YuChuXi <cyu245656@gmail.com>
2025-10-28 12:40:55 +0800

    update

--- start.sh
+++ start.sh
@@ -13,17 +13,16 @@ export CUDNN_BENCHMARK=1
 export CUDA_LAUNCH_BLOCKING=1
 export TORCH_USE_CUDA_DSA=1
 
-python $PWD/main.py --port 8000 --listen 0.0.0.0 \
+python $PWD/main.py --port 48189 --listen 0.0.0.0 \
     --preview-method taesd \
-    --novram \
     --verbose DEBUG \
     --tls-keyfile key.pem --tls-certfile cert.pem \
+    --use-flash-attention \
+    --async-offload \
     $@
-    #--disable-xformer \
-    #--use-flash-attention \
-    #--async-offload \
+    #--disable-xformer \--novram \
+    #--use-quad-cross-attention \
     #--use-split-cross-attention \
     #--use-pytorch-cross-attention \
-    #--use-quad-cross-attention \
     #--use-sage-attention \
 
