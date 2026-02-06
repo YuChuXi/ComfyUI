f5128da884e6fd3bb0bdb67feea0bbebd0c117ff
YuChuXi <cyu245656@gmail.com>
2026-02-07 00:17:36 +0800

    更新启动脚本以使用Sage注意力，移除Flash注意力选项

--- start.sh
+++ start.sh
@@ -17,12 +17,12 @@ python $PWD/main.py --port 48189 --listen 0.0.0.0 \
     --preview-method taesd \
     --verbose DEBUG \
     --tls-keyfile key.pem --tls-certfile cert.pem \
-    --use-flash-attention \
+    --use-sage-attention \
     --async-offload \
     $@
     #--disable-xformer \--novram \
     #--use-quad-cross-attention \
+    #--use-flash-attention \
     #--use-split-cross-attention \
     #--use-pytorch-cross-attention \
-    #--use-sage-attention \
 
