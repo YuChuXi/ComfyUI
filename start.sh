5c1c2def10ac832d36b7c79bd57875f44d3441df
YuChuXi <cyu245656@gmail.com>
2026-03-21 20:02:58 +0800

    Update start.sh to change preview method from taesd to latent2rgb

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ export CUDA_LAUNCH_BLOCKING=1
 export TORCH_USE_CUDA_DSA=1
 
 python $PWD/main.py --port 48189 --listen 0.0.0.0 \
-    --preview-method taesd \
+    --preview-method latent2rgb \
     --verbose DEBUG \
     --tls-keyfile key.pem --tls-certfile cert.pem \
     --async-offload \
